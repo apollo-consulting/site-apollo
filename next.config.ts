4f2d1bedbdfca0ac9b8de7aa34dd5891bf584c0e
apollo-consulting <apollo-consulting@outlook.com>
2026-01-10 11:43:16 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // <--- ADICIONE ESTA LINHA
+  // outras configurações...
 };
 
 export default nextConfig;
